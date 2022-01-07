80ca5ec3606095b4e0b5d56e1de624dae35268ed
Vishal <vishalviz992@gmail.com>
2022-01-07 16:37:05 +0530

    department-service

---
+++ src/main/java/com/vishal/department/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.vishal.department.service;
+
+import com.vishal.department.entity.Department;
+import com.vishal.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside SaveDepartment method of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside findDepartmentById method of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
